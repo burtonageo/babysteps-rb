2e3c5dea68f08e57bf7f928ccff5ec9cf936546f
George Burton <burtonageo@gmail.com>
2015-04-23 20:25:50 +0100

    Exposing the duplication between the tests

--- babysteps.rb
+++ babysteps.rb
@@ -1,15 +1,6 @@
 #! /usr/bin/env ruby
 
 #> Given:
-# A git repo: hello.git
-#     With uncommitted changes
-# A number x
-
-#> When X seconds have elapsed since the script was called
-#> Then all uncommitted changes will be reverted
-
-
-#> Given:
 # A git repo in a different directory: ../otherrepo/hello.git
 #    With uncommitted changes
 # A number x
@@ -26,22 +17,29 @@
 #> When n seconds have elapsed since the last commit and n < x:
 #> Then the uncommited changed will still be there
 
+#> Given:
+# A git repo with no uncommitted changes
+# A number x
+# Last iteration happened 0 seconds ago
+
+#> When the iteration happenes
+#> Then the repository will reverted x seconds past start time when I make a change at 1 seconds elapsed.
 
 #> Given:
 # A git repo with no uncommitted changes
 # A number x
+# Last iteration happened x seconds ago
 
 #> When the user starts the script
-#> Then the repository will reverted 2x seconds past start time when I make a change at
-#  x + 1 seconds elapsed.
+#> Then the repository will reverted x seconds past start time when I make a change at 1 seconds elapsed.
 
 #> Given:
 # A git repo with no uncommitted changes
 # A number x
+# Last iteration happened 2x seconds ago
 
 #> When the user starts the script
-#> Then the repository will reverted 3x seconds past start time when I make a change at
-#  2x + 1 seconds elapsed.
+#> Then the repository will reverted x seconds past start time when I make a change at 1 seconds elapsed.
 
 git_repo = ARGV[0]
 countdown = ARGV[1].to_i
