65cccc016fde079ce088e5fc3ad0c9a7658783e4
George Burton <burtonageo@gmail.com>
2015-04-23 19:08:47 +0100

    Add test 1 (which passes) and test 2 (which is untested).

--- babysteps.rb
+++ babysteps.rb
@@ -1,5 +1,54 @@
 #! /usr/bin/env ruby
 
+#> Given:
+# A git repo: hello.git
+#     With uncommitted changes
+#     The last commit is less than X seconds ago from now
+# A number x
+
+#> When X seconds have elapsed since the last commit and there are uncommitted changes:
+# All uncommitted changes will be reverted
+
+#> Then:
+# There should be no uncommited changes
+
+git_repo = ARGV[0]
+num_seconds = ARGV[1]
+
+`git --git-dir=#{git_repo}/.git --work-tree=#{git_repo} reset --hard HEAD`
+
+#> Given:
+# A git repo in a different directory: ../otherrepo/hello.git
+#    With uncommitted changes
+#    The last commit is less than X seconds ago from now
+# A number x
+
+#> When X seconds have elapsed since the last commit and there are uncommitted changes:
+# All uncommitted changes will be reverted
+
+#> Then:
+# There should be no uncommited changes
+
+#> Given
+# A git repo: hello.git
+# A number x
+# With no uncommitted changes
+
+#> When
+# x seconds have passed since this script has called
+# No modifications to the repo have been made
+
+#> Then
+# The repository should be unchanged
+
+
+
+#take_baby_steps . 120
+
+#Max(X seconds since the last commit, X seconds since script ran) > 120  -> Revert
+
+
+
 # Get a git directory and number of seconds (= x)
 
 
